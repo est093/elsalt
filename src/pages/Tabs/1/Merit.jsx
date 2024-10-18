c67c41b0e49107bb7516a0fe652e99df96623158
est093 <estherij093@naver.com>
2024-10-18 17:01:28 +0900

    mod

--- src/pages/Tabs/1/Merit.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Merit() {
-  return <div>Merit</div>;
-}
--- src/pages/Tabs/1/Notice.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Notice() {
-  return (
-    <div className="notice_wrap">
-      <a href=""></a>
-    </div>
-  );
-}
