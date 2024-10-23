dc33de4740651ca98eb9ab61606a8b59940cd5c7
est093 <estherij093@naver.com>
2024-10-23 16:58:48 +0900

    added_centerscreen

---
+++ src/pages/Tabs/3/Tab3_1.jsx
@@ -0,0 +1,557 @@
+import React from "react";
+import "../../../css/etc.css";
+import "../../../css/carousel.css";
+export default function Tab3_1() {
+  return (
+    <div className="container">
+      <h4 className="general_font">빛과 소금내과 종합검진소개</h4>
+      <div className="pad-bg light-bg-blue mt-3">
+        최근, 삶의 질이 높아지면서 질병의 치료뿐만 아니라 예방과 조기 발견의
+        중요성이 강조되고 있습니다.
+        <br />
+        종합검진은 환자와 가족이 겪을 수 있는 고통을 사전에 대비할 수 있을 뿐만
+        아니라 건강에 대한 불안감에서 벗어나 활기찬 생활을 영위할수 있도록
+        도와줍니다.
+        <br />
+        <br />
+        저희 빛과 소금 종합검진을 통해 웃음이 넘치는 건강한 가정을 이루시기
+        바랍니다
+      </div>
+      <div className="mt-5">
+        <h5 className="mb-5">
+          <span className="bluebg">예약안내</span>
+        </h5>
+        <div>
+          <table className="table_s">
+            <tbody>
+              <tr>
+                <th>종합검진일시</th>
+                <td>
+                  매주 월요일~토요일
+                  <br />( 월, 화, 목, 금 : 오전8:30 ~ 오후 5:30, 수, 토 :
+                  오전8:30 ~ 오후12:30 )
+                </td>
+              </tr>
+              <tr>
+                <th>소요시간</th>
+                <td>2~3시간</td>
+              </tr>
+              <tr>
+                <th>예약방법</th>
+                <td>
+                  전화 혹은 방문 예약시 검사에 필요한 유의사항과 검진 일시를
+                  상세히 알려 드림
+                </td>
+              </tr>
+              <tr>
+                <th>예약시 필요사항</th>
+                <td>성명, 주민등록번호, 주소, 전화번호(휴대폰번호 포함)</td>
+              </tr>
+              <tr>
+                <th>예약시간</th>
+                <td>
+                  매주 월요일 ~ 토요일
+                  <br />( 월, 화, 목, 금 : 오전8:30 ~ 오후 5:30, 수, 토 :
+                  오전8:30 ~ 오후12:30 )
+                </td>
+              </tr>
+              <tr>
+                <th>전화예약</th>
+                <td>031-905-8175 / 031-905-8176</td>
+              </tr>
+              <tr>
+                <th>방문예약</th>
+                <td>빛과 소금내과 접수처</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h5 className="mb-5">
+          <span className="bluebg">주차안내</span>
+        </h5>
+        병원 뒤 이면도로 이용
+      </div>
+      <div className="mt-5">
+        <h5 className="mb-5">
+          <span className="bluebg">검진 시 유의사항</span>
+        </h5>
+        <div>
+          <table className="table_s">
+            <tbody>
+              <tr>
+                <th>① 검사 하루 전 유의 사항</th>
+                <td>
+                  저녁식사는 오후 8시 이전에 드십시오.
+                  <br />
+                  오후9시 이후에는 물을 포함하여 금식하셔야 합니다.
+                  <br />
+                  혈압약은 평소대로 복용하시고, 충분한 수면을 취하십시오.
+                </td>
+              </tr>
+              <tr>
+                <th>② 검사당일 주의사항</th>
+                <td>
+                  아침식사는 물론 술, 담배,커피,음료수 등 아무것도 드시지
+                  마십시오.
+                  <br />
+                  양치질은 무방하며, 귀중품은 소지하지 마시고 오세요.
+                  <br />
+                  혈압약을 복용하고 오세요
+                </td>
+              </tr>
+              <tr>
+                <th>③ 직원들에게 알려주어야 할 사항</th>
+                <td>
+                  임산부는 검진 시 간호사에게 미리 말씀해 주십시오.
+                  <br />
+                  생리가 시작되거나 생리중인 여성분께서도 미리 알려주십시오.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h5 className="mb-5">
+          <span className="bluebg">검진판정 및 상담안내</span>
+        </h5>
+        <div>
+          <ul className="intro_list pad-5-0-0-4">
+            <li>검진과 동시에 진료를 받을 수 있습니다.(one-stop-system)</li>
+            <li>검진결과는 7일이내 종합검진 책자로 만들어 상담해 드립니다.</li>
+          </ul>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h5 className="mb-5">
+          <span className="bluebg">종합검진의 종목</span>
+        </h5>
+        <div>
+          <table className="table_s">
+            <tbody>
+              <tr>
+                <th>
+                  신체계측
+                  <br />
+                  (Anthropometry Exam)
+                </th>
+                <td>- 신장, 체중, 비만도</td>
+              </tr>
+
+              <tr>
+                <th>
+                  청력검사
+                  <br />
+                  (Audiologic Exam)
+                </th>
+                <td>- 청력</td>
+              </tr>
+
+              <tr>
+                <th>
+                  안과검사
+                  <br />
+                  (Ophthalmologic Exam)
+                </th>
+                <td>- 시력, 색약</td>
+              </tr>
+
+              <tr>
+                <th>
+                  호흡기검사
+                  <br />
+                  (Pulmonologic Exam)
+                </th>
+                <td>- 폐기능검사 및 흉부촬영</td>
+              </tr>
+
+              <tr>
+                <th>
+                  순환기검사
+                  <br />
+                  (Cardiologic Exam)
+                </th>
+                <td>- 혈압, 맥박, 심전도</td>
+              </tr>
+
+              <tr>
+                <th>
+                  소화기검사
+                  <br />
+                  (Gastrointestinal Exam)
+                </th>
+                <td>- 전자 위내시경, 수면내시경, 대변검사</td>
+              </tr>
+
+              <tr>
+                <th>
+                  혈액학검사
+                  <br />
+                  (Hematologic Exam)
+                </th>
+                <td>
+                  - 백혈구, 적혈구, 혈소판검사 및 적혈구 침강속도, 빈혈검사
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  생화학검사
+                  <br />
+                  (Biochemistry)
+                </th>
+                <td>
+                  - 혈당, 간기능, 지질, 신장기능, 간염, 부갑상선기능, 통풍검사
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  전해질 및 무기질 검사
+                  <br />
+                  (Electrolyte)
+                </th>
+                <td>- Na, K, Cl, P, Ca</td>
+              </tr>
+
+              <tr>
+                <th>
+                  요검사
+                  <br />
+                  (Urinalysis)
+                </th>
+                <td>
+                  - 당뇨, 단백뇨, 혈뇨, 빌리루빈뇨, 방광염 등의 비뇨기계
+                  염증검사
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  혈청면역학검사
+                  <br />
+                  (Serology)
+                </th>
+                <td>
+                  - B형간염, C형간염, 매독반응, 류마티스인자검사, 에이즈검사
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  내분비검사
+                  <br />
+                  (Endocrinologic Exam)
+                </th>
+                <td>- 갑상선기능검사(T3, free T4, TSH)</td>
+              </tr>
+
+              <tr>
+                <th>
+                  방사선검사
+                  <br />
+                  (Radiologic Exam)
+                </th>
+                <td>- 관절염 및 척추질환시 병변관절 및 척추검사</td>
+              </tr>
+
+              <tr>
+                <th>
+                  골다공증
+                  <br />
+                  (Osteoporotic Exam)
+                </th>
+                <td>- 골밀도 측정(BMD)</td>
+              </tr>
+
+              <tr>
+                <th>
+                  체성분검사
+                  <br />
+                  (Body Composition Analyzer)
+                </th>
+                <td>
+                  - 비만진단(부위별 체지방 및 근육량평가), 영양진단, 체수분검사,
+                  (부종진단) 및 신체의 균형상태검사
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  경동맥 초음파
+                  <br />( carotid Ultrasonography)
+                </th>
+                <td>
+                  - 뇌에 혈액을 공급하는 경동맥의 동맥경화 여부,상태 및
+                  혈액흐름을 평가
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  심장 초음파
+                  <br />
+                  (echo Ultrasonography)
+                </th>
+                <td>
+                  - 심장의 크기, 기능, 두께, 심근허혈, 심장판만이상, 선천성
+                  심장질환 평가
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  유방 초음파검사
+                  <br />
+                  (Breast Ultrasonography)
+                </th>
+                <td>- 유방암 검사</td>
+              </tr>
+
+              <tr>
+                <th>
+                  자궁 및 난소암 초음파 검사
+                  <br />
+                  (Pevic Ultrasonography)
+                </th>
+                <td>- 자궁, 난소암 검사</td>
+              </tr>
+
+              <tr>
+                <th>
+                  갑상선 초음파 검사
+                  <br />
+                  (Thyroid Ultrasonography)
+                </th>
+                <td>- 갑상선결절 및 종양 등을 검사</td>
+              </tr>
+
+              <tr>
+                <th>
+                  복부초음파 검사
+                  <br />
+                  (Abdomen Ultrasonography)
+                </th>
+                <td>
+                  - 간, 비장, 췌장, 담낭 및 담도, 신장 등 복부의 장기 검사
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  관절초음파 검사
+                  <br />
+                  (Joint Ultrasonogaphy)
+                </th>
+                <td>- 무릎, 어깨, 발목 등(관절염환자)</td>
+              </tr>
+
+              <tr>
+                <th>
+                  암검사(종양표지자)
+                  <br />
+                  (Tumor maker)
+                </th>
+                <td>
+                  - AFP(간암,간경화), Ferritin(기타 악성종양 및 빈혈),
+                  CEA(대장암), CA19-9(췌장암), PSA(전립선암)(男), CA125(난소암,
+                  자궁내막암, 자궁근종)(女)
+                </td>
+              </tr>
+
+              <tr>
+                <th>
+                  자가면역질환검사
+                  <br />
+                  (Autoimmune disease)
+                </th>
+                <td>- ANA(자가면역항체) 등</td>
+              </tr>
+
+              <tr>
+                <th>종합판정 및 상담</th>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="mt-5">
+        <h5 className="mb-5">
+          <span className="bluebg">빛과소금 건강검진 순서</span>
+        </h5>
+
+        <div
+          id="oc-images"
+          className="owl-carousel image-carousel carousel-widget mb-6"
+          data-items-xs="2"
+          data-items-sm="3"
+          data-items-lg="4"
+          data-items-xl="5"
+        >
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_1.jpg" alt="Image 1" />
+            <div>
+              <span className="general_font font-size-20-16">상담 및 진찰</span>
+              <br />
+              <div className="font-size-17-14">
+                전문의 상담 및 진찰을 통해 검진방법과 선택검사를 결정합니다.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_2.jpg" alt="Image 2" />
+            <div>
+              <span className="general_font font-size-20-16">
+                신체계측 및 혈압체크
+              </span>
+              <br />
+              <div className="font-size-17-14">
+                신장, 몸무게 등을 측정하여 비만도를 파악하고 혈압 및 맥박 측정.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_3.jpg" alt="Image 3" />
+            <div>
+              <span className="general_font font-size-20-16">안과검사</span>
+              <br />
+              <div className="font-size-17-14">
+                시력검사 뿐만아니라 색맹여부를 파악합니다.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_4.jpg" alt="Image 4" />
+            <div>
+              <span className="general_font font-size-20-16">청력검사</span>
+              <br />
+              <div className="font-size-17-14">
+                청력검사를 실행하여 귀의 이상 유무 진단.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_5.jpg" alt="Image 5" />
+            <div>
+              <span className="general_font font-size-20-16">
+                채혈 및 채뇨검사
+              </span>
+              <br />
+              <div className="font-size-17-14">
+                혈액분석기와 각종측정기를 통해 80~90여종의 검사.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_6.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">심전도검사</span>
+              <br />
+              <div className="font-size-17-14">
+                심장의 움직임을 기록하는 검사법. 심근경색, 부정맥, 협심증 등
+                심장질환 진단.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_7.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">폐기능검사</span>
+              <br />
+              <div className="font-size-17-14">
+                폐활량 등의 폐의 기능을 평가하여 천식 등의 기관지 및 폐질환
+                진단.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_8.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">골밀도검사</span>
+              <br />
+              <div className="font-size-17-14">골다공증 진단.</div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_10.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">방사선검사</span>
+              <br />
+              <div className="font-size-17-14">
+                흉부촬영과 복부, 각종 관절부위 촬영.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_11.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">체성분검사</span>
+              <br />
+              <div className="font-size-17-14">
+                비만진단(부위별 체지방 및 근육량 평가), 영양상태,
+                체수분검사(부종진단)및 신체의 균형상태 검사.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_12.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">
+                복부초음파 검사
+              </span>
+              <br />
+              <div className="font-size-17-14">
+                복부의 간, 신장, 췌장, 비장, 담낭, 방광, 자궁, 난소 (여자),
+                전립선(남자) 등의 각종 장기 검사.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_13.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">
+                유방 초음파
+                <br />
+                갑상선 초음파
+                <br />
+                경동맥 초음파
+                <br />
+                심장 초음파
+              </span>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_14.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">
+                전자 위내시경
+              </span>
+              <br />
+              <div className="font-size-17-14">
+                (선택:수면내시경)
+                <br />
+                식도, 위. 십이지장 등의 염증이나 궤양, 조기암 진단.
+              </div>
+            </div>
+          </div>
+          <div className="oc-item place-font-center">
+            <img src="/elsalt/elsalt/3_1_15.jpg" alt="Image" />
+            <div>
+              <span className="general_font font-size-20-16">
+                결과상담 및 처방
+              </span>
+              <br />
+              <div className="font-size-17-14">
+                내과전문의가 직접 검진결과를 종합하여 건강상태를 판정해 드리고
+                질병이 발견되는 즉시 치료 및 추적
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
